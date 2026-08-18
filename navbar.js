cd1d1c433d31c262069eab6a3cb04d52aab73cfc
harfieyly <harfieyly@gmail.com>
2026-08-18 19:22:53 +0800

    4

--- navbar.js
+++ navbar.js
@@ -1,13 +1,9 @@
 class NavbarComponent extends HTMLElement {
     connectedCallback() {
-        // Kesan lokasi fail HTML semasa
-        const isRoot = window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('/');
-        const prefix = isRoot ? '.' : '..';
-
         this.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top custom-navbar" style="background-color: #121212;">
             <div class="container">
-                <a class="navbar-brand fw-bold" href="${prefix}/index.html">
+                <a class="navbar-brand fw-bold" href="./index.html">
                     <span style="color: #7b12ff;">klia2</span>.info
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -15,23 +11,23 @@ class NavbarComponent extends HTMLElement {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="${prefix}/index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./index.html">Home</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Travel</a>
                             <ul class="dropdown-menu dropdown-menu-dark" style="background-color: #262626;">
-                                <li><a class="dropdown-item" href="${prefix}/Airport_Page/Airport.html">Airport</a></li>
-                                <li><a class="dropdown-item" href="${prefix}/Airline_Page/airlines.html">Airline</a></li>
+                                <li><a class="dropdown-item" href="./Airport_Page/Airport.html">Airport</a></li>
+                                <li><a class="dropdown-item" href="./Airline_Page/airlines.html">Airline</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Transportation</a>
                             <ul class="dropdown-menu dropdown-menu-dark" style="background-color: #262626;">
-                                <li><a class="dropdown-item" href="${prefix}/Public_Page/public.html">Public Transportation</a></li>
-                                <li><a class="dropdown-item" href="${prefix}/Expressway_Page/expressways.html">Expressways</a></li>
+                                <li><a class="dropdown-item" href="./Public_Page/public.html">Public Transportation</a></li>
+                                <li><a class="dropdown-item" href="./Expressway_Page/expressways.html">Expressways</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><a class="nav-link" href="${prefix}/Hotel_Page/Hotel.html">Hotel</a></li>
-                        <li class="nav-item"><a class="nav-link" href="${prefix}/Attraction_Page/Attraction.html">Attraction</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./Hotel_Page/Hotel.html">Hotel</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./Attraction_Page/Attraction.html">Attraction</a></li>
                     </ul>
                 </div>
             </div>
